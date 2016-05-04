ca73ec14db1f571a90ab6b826dc35ebf5952a779
thanhtungdp <tungptkh@gmail.com>
2016-05-04 18:46:10 +0700

    update readme.md

--- app/components/partials/Header.js
+++ app/components/partials/Header.js
@@ -7,7 +7,7 @@ export default class Header extends Component {
             <Navbar className="navbar-custom" inverse>
                 <Navbar.Header>
                     <Navbar.Brand>
-                        <a href="#"><i className="fa fa-home"></i>Seach app</a>
+                        <a href="#"><i className="fa fa-home"></i> Seach app</a>
                     </Navbar.Brand>
                     <Navbar.Toggle />
                 </Navbar.Header>
@@ -16,7 +16,7 @@ export default class Header extends Component {
                         <NavItem eventKey={4} href="#/">Search</NavItem>
                         <NavItem eventKey={4} href="#/search/annimal">Search test 1</NavItem>
                         <NavItem eventKey={4} href="#/search/lion">Search test 2</NavItem>
-
+                        <NavItem eventKey={4} href="https://github.com/thanhtungdp/redux-500api"><i className="fa fa-github"></i> Github</NavItem>
                         <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
                             <MenuItem eventKey={3.3}>Something else here</MenuItem>
                             <MenuItem divider/>
